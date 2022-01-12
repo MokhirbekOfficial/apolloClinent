4c341e9e7ddbce41197b26afb03755a95a6e4a90
MokhirbekOfficial <77888062+MokhirbekOfficial@users.noreply.github.com>
2022-01-12 19:59:13 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useQuery, useMutation, useSubscription, gql} from '@apollo/client'
+
+const SCHEME = gql`
+query users($id: ID!){
+  users(id: $id){
+    id
+    name
+  }
+}
+`
+const MUTATION_SCHEME = gql`
+mutation deleteUser($id: ID!){
+  deleteUser(id: $id)
+}
+`
+
+const AllUsers = gql`
+query{
+  allUsers{
+    id
+    name
+  }
+}
+`
+
+const SUBSCRIPTION = gql`
+subscription{
+  getUsers{
+    id
+    name
+  }
+}
+`
 
 function App() {
+  const {data} = useQuery(AllUsers)
+
+  const [deleteUser, {data: mutData}] = useMutation(MUTATION_SCHEME,{
+    update: (cache, data) =>{
+      console.log(data)
+    }
+  })
+
+  useSubscription(SUBSCRIPTION, {
+    onSubscriptionData: ({client: {cache}, SubscriptionData: {data}})=>{
+      cache.modify({
+        fields:{
+          allUsers: (allUsers = [])=>{}
+        }
+      })
+    },
+  })
+
+  const render = (id)=>{
+    deleteUser({
+      variables:{
+        id
+      }
+    })
+    console.log(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        data && data.allUsers.map(e => (
+          <div key={e.id}>
+            <h1>{e.name}</h1>
+            <button onClick={()=>render(e.id)}>Delete</button>
+          </div>
+        ))
+      }
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, split} from '@apollo/client'
+import {getMainDefinition} from '@apollo/client/utilities'
+import {WebSocketLink} from '@apollo/client/link/ws'
+import {setContext} from '@apollo/client/link/context'
+import { BrowserRouter as Router} from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+})
+
+const wsLink = () => {
+  const token = localStorage.getItem ('token');
+  return new WebSocketLink({
+    uri: 'ws://localhost:4000/graphql',
+    options: {
+      reconnect: true,
+      timeout: 30000
+    }
+  })
+}
+
+const authLink = setContext(async(_, {headers})=>{
+  const token = await localStorage.getItem('token')
+  return{
+    headers: {
+      ...headers,
+      authorithation: token ? `${token}`: "",
+    }
+  }
+});
+
+const splitLink =split(
+  ({query})=>{
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' && 
+      definition.operation === 'subscribtion'
+    );
+  },
+  wsLink(),
+  authLink.concat(httpLink)
+)
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
